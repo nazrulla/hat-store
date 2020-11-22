56c10db23bfbb54048f9a85198a0f33c27d394a7
Nazrulla Sadullaev <nsadullaev98@gmail.com>
2020-11-23 01:19:16 +0500

    Add add to cart button and style it

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component'
 
 const CollectionItem = ({ id, name, price, imageUrl}) => (
     <div className='collection-item'>
@@ -12,6 +13,7 @@ const CollectionItem = ({ id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
+        <CustomButton>Add to Cart</CustomButton>
     </div>
 )
 
